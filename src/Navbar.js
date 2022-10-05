45b49277d25c7eb31341d83b4090742b5ae2e60e
Duckoboye <swedishshadows@gmail.com>
2022-10-05 13:22:45 +0200

    Fränt cafe bro

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav className="nav">
+			<Link to="/" className="www-title">
+				Fränt Café™
+			</Link>
+			<ul>
+				<NavLink to="/menu" className="nav-item">
+					Menu
+				</NavLink>
+				<NavLink to="/about" className="nav-item">
+					About
+				</NavLink>
+				<NavLink to="/contact" className="nav-item">
+					Contact
+				</NavLink>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
